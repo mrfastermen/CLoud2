6298a23cf48e331b5415fa623ea3f37fc8ffae38
mrfastermen <mrfastermen@gmail.com>
2024-10-05 10:58:44 -0500

    Actualiza proyecto

--- ec2.tf
+++ ec2.tf
@@ -23,8 +23,25 @@ resource "aws_instance" "cloud2-instance" {
 
   # Seguridad de la instancia (acceso solo por SSH - puerto 22)
   vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "cloud2-instance"
+  }
+}
+
+resource "aws_instance" "cloud2-instance2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.deployer.key_name
+
+  subnet_id = aws_subnet.public2.id # Asociar la instancia a la subred
+
+  # Seguridad de la instancia (acceso solo por SSH - puerto 22)
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "cloud2-instance2"
   }
 }
